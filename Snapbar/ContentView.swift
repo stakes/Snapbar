f51c418223437195d7b4c0dfd7bb64a14d95fea0
Jay Stakelon <stakes@users.noreply.github.com>
2020-09-05 13:17:42 -0700

    Initial Commit

---
+++ Snapbar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Snapbar
+//
+//  Created by Jay Stakelon on 9/5/20.
+//  Copyright © 2020 Jay Stakelon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
