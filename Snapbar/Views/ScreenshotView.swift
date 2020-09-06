01327c4366bfd156f7fbaf8e62107e4336b6339f
Jay Stakelon <stakes@users.noreply.github.com>
2020-09-06 14:23:25 -0700

    if an image goes missing, don't break

--- Snapbar/Views/ScreenshotView.swift
+++ Snapbar/Views/ScreenshotView.swift
@@ -11,15 +11,24 @@ import SwiftUI
 struct ScreenshotView: View {
     @ObservedObject var viewModel: ScreenshotViewModel
     var body: some View {
-        HStack {
-            Image(nsImage: NSImage(contentsOf: viewModel.screenshot.url)!).resizable().frame(width: 160, height: 90)
-        }.onDrag {
-            let data = NSImage(contentsOf: self.viewModel.screenshot.url)!
-            let provider = NSItemProvider(contentsOf: self.viewModel.screenshot.url)
-            provider?.previewImageHandler = { (handler, _, _) -> Void in
-                handler?(data as NSSecureCoding?, nil)
+        Group {
+            if (NSImage(contentsOf: viewModel.screenshot.url) != nil) {
+                HStack {
+                    Image(nsImage: NSImage(contentsOf: viewModel.screenshot.url)!).resizable().frame(width: 160, height: 90)
+                }.onDrag {
+                    let data = NSImage(contentsOf: self.viewModel.screenshot.url)!
+                    let provider = NSItemProvider(contentsOf: self.viewModel.screenshot.url)
+                    provider?.previewImageHandler = { (handler, _, _) -> Void in
+                        handler?(data as NSSecureCoding?, nil)
+                    }
+                    return provider!
+                }
+            } else {
+                // missing image
+                HStack {
+                    Text("missing image")
+                }
             }
-            return provider!
         }
     }
 }
